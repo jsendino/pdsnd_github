962a2ffa36506ded9d6e2814809d9b810c726448
Sendino Jorge (McKinsey) <jorge.sendino@partners.fortum.com>
2020-11-02 16:17:15 +0100

    Added comma at the end of dict

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = {
     'chicago': 'chicago.csv',
     'new york city': 'new_york_city.csv',
-    'washington': 'washington.csv' 
+    'washington': 'washington.csv',
 }
 
 def get_filters():
